0675c41165856a218d21daf2810d5b592c723249
Artemowkin <artyom.groshev2017@gmail.com>
2021-08-13 15:16:19 +0900

    Added command to get all product reviews

--- shop/services/commands.py
+++ shop/services/commands.py
@@ -1,7 +1,12 @@
+from uuid import UUID
+
 from generic.services.commands import BaseGetAllCommand, BaseCreateCommand
 
-from .base import ProductsGetService, ProductCreateService
-from ..serializers import ProductSerializer
+from .base import (
+	ProductsGetService, ProductCreateService, ProductReviewsGetService,
+	count_overall_rating
+)
+from ..serializers import ProductSerializer, ProductReviewSerializer
 
 
 class GetAllProductsCommand(BaseGetAllCommand):
@@ -16,3 +21,38 @@ class CreateProductCommand(BaseCreateCommand):
 
 	create_service_class = ProductCreateService
 	serializer_class = ProductSerializer
+
+
+class GetAllProductReviewsCommand:
+	"""Base command to get all entries"""
+
+	get_product_service_class = ProductsGetService
+	get_review_service_class = ProductReviewsGetService
+	serializer_class = ProductReviewSerializer
+
+	def __init__(self, product_pk: UUID):
+		self._product_pk = product_pk
+		self._get_product_service = self.get_product_service_class()
+
+	def _get_all_product_reviews(self):
+		"""Get a concrete product and return all reviews on this product"""
+		product = self._get_product_service.get_concrete(self._product_pk)
+		get_review_service = self._get_review_service_class(product)
+		all_product_reviews = get_review_service.get_all()
+		return all_product_reviews
+
+	def execute(self) -> tuple[dict, int]:
+		"""
+		Get all product reviews and return serialized list with these
+		reviews, and status code of response
+		"""
+		all_product_reviews = self._get_all_product_reviews()
+		overall_rating = count_overall_rating(all_product_reviews)
+		serialized_reviews = self.serializer_class(
+			all_product_reviews, many=True
+		)
+		response_data = {
+			'overall_rating': overall_rating,
+			'reviews': serialized_reviews.data
+		}
+		return response_data, 200
