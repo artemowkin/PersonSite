1d672fd3e06c16dbd111fef16cb95e425202ae39
Artemowkin <artyom.groshev2017@gmail.com>
2021-08-14 13:55:58 +0900

    Divided posts facades

--- posts/services/facades.py
+++ posts/services/facades.py
@@ -1,16 +1,45 @@
-from generic.services.facades import BaseAPICRUDFacade
+from generic.services.facades import (
+	BaseGetFacade, BaseCreateFacade, BaseUpdateFacade, BaseDeleteFacade
+)
 from .base import (
 	PostGetService, PostCreateService, PostUpdateService, PostDeleteService
 )
 from ..serializers import PostSerializer
 
 
-class PostCRUDFacade(BaseAPICRUDFacade):
-	"""Facade with CRUD functionality using services"""
+class PostGetFacade(BaseGetFacade):
+	"""Facade to get posts using services"""
+
 	serializer_class = PostSerializer
 
 	def __init__(self):
 		self.get_service = PostGetService()
+
+
+class PostCreateFacade(BaseCreateFacade):
+	"""Facade to create a new post using services"""
+
+	serializer_class = PostSerializer
+
+	def __init__(self):
 		self.create_service = PostCreateService()
+
+
+class PostUpdateFacade(BaseUpdateFacade):
+	"""Facade to update a concrete post using services"""
+
+	serializer_class = PostSerializer
+
+	def __init__(self):
+		self.get_service = PostGetService()
 		self.update_service = PostUpdateService()
-		self.delete_service = PostDeleteService()
+
+
+class PostDeleteFacade(BaseDeleteFacade):
+	"""Facade to delete a concrete post using services"""
+
+	serializer_class = PostSerializer
+
+	def __init__(self):
+		self.get_service = PostGetService
+		self.delete_service = PostDeleteService
