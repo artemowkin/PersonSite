8b90df8f2e4b735a201769710f65f6155c73c318
Artemowkin <artyom.groshev2017@gmail.com>
2021-08-15 20:08:56 +0900

    Added Order model to admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+	list_display = (
+		'first_name', 'last_name', 'city', 'address', 'postal_code', 'status',
+		'pub_date'
+	)
+	list_filter = ('status',)
+	search_fields = ('first_name', 'last_name', 'city', 'address')
+	raw_id_fields = ('customer',)
